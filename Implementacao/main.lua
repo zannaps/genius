37eb4fa9ead56f0cee650a6e51f1649abc7b0af4
zannaps <zannaps@gmail.com>
2014-09-23 10:08:22 -0300

    Versão 1.1
    Implementação dos sons das teclas

--- Implementacao/main.lua
+++ Implementacao/main.lua
@@ -9,10 +9,16 @@ local teclaLa = display.newImage("imagens/tecla_6.gif", 700, posicaoImagemY)
 local teclaSi = display.newImage("imagens/tecla_7.gif", 832, posicaoImagemY)
 local teclaDo2 = display.newImage("imagens/tecla_8.gif", 965, posicaoImagemY)
 
-notaDo = audio.loadStream("sons/do8.mp3")
+notaDo = audio.loadStream("sons/do.wav")
 notaRe = audio.loadStream("sons/re.wav")
+notaMi = audio.loadStream("sons/mi.wav")
+notaFa = audio.loadStream("sons/fa.wav")
+notaSol = audio.loadStream("sons/sol.wav")
+notaLa = audio.loadStream("sons/la.wav")
+notaSi = audio.loadStream("sons/si.wav")
+notaDo2 = audio.loadStream("sons/mi")
 
-function tocarNoda(event)
+function tocarNota(event)
 	if (event.target == teclaDo) then
     	audio.play(notaDo);
     end
@@ -20,32 +26,32 @@ function tocarNoda(event)
     	audio.play(notaRe);
     end
     if (event.target == teclaMi) then
-    	print("não implementado")
+    	audio.play(notaMi);
     end
     if (event.target == teclaFa) then
-    	print("não implementado")
+    	audio.play(notaFa);
     end
     if (event.target == teclaSol) then
-    	print("não implementado")
+    	audio.play(notaSol)
     end
     if (event.target == teclaLa) then
-    	print("não implementado")
+    	audio.play(notaLa);
     end
     if (event.target == teclaSi) then
-    	print("não implementado")
+    	audio.play(notaSi);
     end
     if (event.target == teclaDo2) then
     	print("não implementado")
     end
 end
 
-teclaDo:addEventListener("touch", tocarNoda)
-teclaRe:addEventListener("touch", tocarNoda)
-teclaMi:addEventListener("touch", tocarNoda)
-teclaFa:addEventListener("touch", tocarNoda)
-teclaSol:addEventListener("touch", tocarNoda)
-teclaLa:addEventListener("touch", tocarNoda)
-teclaSi:addEventListener("touch", tocarNoda)
-teclaDo2:addEventListener("touch", tocarNoda)
+teclaDo:addEventListener("touch", tocarNota)
+teclaRe:addEventListener("touch", tocarNota)
+teclaMi:addEventListener("touch", tocarNota)
+teclaFa:addEventListener("touch", tocarNota)
+teclaSol:addEventListener("touch", tocarNota)
+teclaLa:addEventListener("touch", tocarNota)
+teclaSi:addEventListener("touch", tocarNota)
+teclaDo2:addEventListener("touch", tocarNota)
 
 
